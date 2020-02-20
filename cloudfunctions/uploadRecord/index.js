3a3639adb105c150f7a200702eb3f885481f24ab
Kenny <Kenny.YB.Chan@outlook.com>
2020-02-20 17:14:14 +0800

    修复云函数安全风险

--- cloudfunctions/uploadRecord/index.js
+++ cloudfunctions/uploadRecord/index.js
@@ -8,6 +8,12 @@ exports.main = async (event, context) => {
   console.log("【uploadRecord】【event参数】", event, "【end】")
   //向Records集合添加记录
   var res = {};
+  const {
+    OPENID,
+    APPID,
+    UNIONID,
+    ENV,
+  } = cloud.getWXContext()
   try {
     res = await db.collection('Records').add({
       // data 字段表示需新增的 JSON 数据
@@ -27,7 +33,7 @@ exports.main = async (event, context) => {
   //向User集合添加记录
   //先获取记录的id
   var userInfo = await db.collection('User').where({
-    _openid: event.userOpenid // 填入当前用户 openid
+    _openid: OPENID // 填入当前用户 openid
   }).get()
   console.log("【uploadRecord】【user的信息】", userInfo, "【end】")
 
@@ -42,21 +48,21 @@ exports.main = async (event, context) => {
   //向Community集合添加记录
   //先获取记录的id
   var comInfo = await db.collection('Community').where({
-    _id: event.comOpenid // 填入当前用户 openid
+    _id: event.comOpenid 
   }).get()
   console.log("【uploadRecord】【com的信息】", comInfo, "【end】")
 
   var comid = comInfo.data[0]._id
   //添加记录
-  var result = db.collection('Community').doc(comid).update({
+  var result = await db.collection('Community').doc(comid).update({
     data: {
       records: _.unshift(res._id)
     }
   })
-  console.log("【uploadRecord】【result的信息】", result, res, "【end】")
+  console.log("【uploadRecord】【result的信息】", result, 'test', res, "【end】")
 
   //更新com的人数信息
-  var r = cloud.callFunction({
+  var r = await cloud.callFunction({
     // 要调用的云函数名称
     name: 'uploadMemberNum',
     // 传递给云函数的参数
@@ -65,7 +71,7 @@ exports.main = async (event, context) => {
       comid: comid
     }
   })
-  console.log("【uploadRecord】【r】", r,comid,event.flag, "【end】")
+  console.log("【uploadRecord】【r】", r, comid, event.flag, "【end】")
 
 
   return res._id
